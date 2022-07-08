111383c742726fcfaf3a630585c9987312e0512c
stephane mbenga <mbengastephane34@gmail.com>
2022-07-07 23:35:52 -0400

    revised user login security

--- lifetracker-api/app.js
+++ lifetracker-api/app.js
@@ -5,15 +5,22 @@ const appTest = require("./app.test")
 const auth = require("./routes/auth")
 const nutrition = require("./routes/nutrition")
 const activity = require("./routes/activity")
+const security = require("./middleware/security")
 const { NotFoundError } = require('./utils/errors')
 
 
 const app = express()
 
+//log request info
 app.use(morgan('tiny'))
+//
+app.use(security.extractUserFromJwt)
+
 app.use(express.json())
 app.use(express.urlencoded());
 app.use(cors())
+//for every request check if a token exists in the authorization header
+//if it does attach the decoded user to res.locals
 app.use("/tests", appTest)
 
 
--- lifetracker-api/middleware/security.js
+++ lifetracker-api/middleware/security.js
@@ -1,12 +1,44 @@
-const { BadRequestError } = require("../utils/errors")
+const jwt = require("jsonwebtoken")
+const {SECRET_KEY} = require("../config")
+const {  UnauthorizedError, BadRequestError } = require("../utils/errors")
 const {validateToken} = require("../utils/tokens")
 
-function authenticateToken (req, res, next){
-    const authHeader = req.headers['authentication']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (token === null){throw new BadRequestError("No header passed when trying to access user info")}
-    validateToken(token, next)
+const jwtFrom = ({headers}) => {
+    if(headers?.authorization){
+        const [scheme, token] = headers.authorization.split(" ")
+        if(scheme ===  "Bearer"){
+            return token
+        }
+    }
 
+    return undefined
 }
 
-module.exports = authenticateToken
+const extractUserFromJwt = (req,res,next)=> {
+    try  {
+        const token = jwtFrom(req)
+        console.log("current token", token)
+        if (token) {
+            console.log("hey")
+            res.locals.user = validateToken(token)
+        }
+        return next()
+    } catch(error){
+        return next()
+    }
+}
+
+function requireAuthenticatedUser (req, res, next){
+    try {
+        const {user} = res.locals
+        console.log("currentUser", user)
+        if(!user?.email) {
+            throw new UnauthorizedError("Unathorized User")
+        }
+        return next()
+    } catch (error){
+        return next(error)
+    }
+}
+
+module.exports = {extractUserFromJwt, requireAuthenticatedUser} 
